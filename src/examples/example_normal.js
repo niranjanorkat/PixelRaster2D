4c5d3215a48b8ac8cd9a07e689c9247ebfdc4c0b
Niranjan Krishna <niranjankrishna.acad@gmail.com>
2025-04-29 04:22:06 +0530

    edited examples

--- src/examples/example_normal.js
+++ src/examples/example_normal.js
@@ -1,20 +1,19 @@
 const canvas = new Canvas(1080, 1080, 10, "canvas");
 
-
 drawPolygon([
-    [20, 20],
-    [40, 25],
-    [35, 40],
-    [15, 40],
-    [10, 25]
+    [20, 10],
+    [40, 15],
+    [35, 30],
+    [15, 30],
+    [10, 15]
 ], BLACK);
 
-drawRectangle(60, 20, 20, 15, BLACK);
+drawRectangle(60, 10, 20, 15, BLACK);
 
-drawTriangle(20, 60, 40, 60, 30, 80, BLACK);
+drawTriangle(20, 50, 40, 50, 30, 70, BLACK);
 
-drawCircle(70, 65, 10, BLACK);
+drawCircle(70, 55, 10, BLACK);
 
-drawEllipse(25, 100, 12, 8, BLACK);
+drawEllipse(25, 90, 12, 8, BLACK);
 
-drawArc(75, 100, 10, 10, 0, Math.PI, BLACK);
+drawArc(75, 90, 10, 10, 0, Math.PI, BLACK);
